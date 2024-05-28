da7a0226347575b718c99e610292568f58045119
Christian Larrea <crlarrea@protonmail.com>
2024-05-28 21:25:30 +0200

    add links to social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../assets/img/eh9_logo.webp";
-import { WeAreOpen } from "./WeAreOpen";
+import { FaTiktok, FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
@@ -9,16 +10,30 @@ export const Footer = () => {
         <h2>opening times</h2>
         <div>
           <p>
-            <span>Mon - Fri</span> 9 am - 5 pm
+            <span>Mon - Fri:</span> 9 AM - 5 PM
           </p>
           <p>
-            <span>Sat - Sun</span> 10 am - 4 pm
+            <span>Sat - Sun:</span> 10 AM - 4 PM
           </p>
         </div>
       </article>
-      <article></article>
+      <article>
+        <h3>Follow us on</h3>
+        <div>
+          <Link
+            to="https://www.instagram.com/eh9.espresso/?hl=en"
+            target="_blank"
+          >
+            <FaInstagram />
+          </Link>
+          <Link to="https://www.tiktok.com/@eh9.espresso" target="_blank">
+            <FaTiktok />
+          </Link>
+        </div>
+      </article>
       <article>
         <img src={logo} />
+        <div></div>
       </article>
     </>
   );
