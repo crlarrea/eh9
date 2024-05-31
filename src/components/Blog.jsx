e2bf664da682f70195a120976c2fda863ef64612
Christian Larrea <crlarrea@protonmail.com>
2024-05-31 12:37:17 +0200

    add articles endpoint to mongo db

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -6,9 +6,42 @@ export const Blog = () => {
   const [articles, setArticles] = useState([]);
   const navigate = useNavigate();
   const getArticles = async () => {
-    const data = await getData("http://127.0.0.1:3900/api/articles", "GET");
-    setArticles(data.data.sort());
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append(
+      "Authorization",
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiYWFzX2RldmljZV9pZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImJhYXNfZG9tYWluX2lkIjoiNjY1OGVlYWRlNjk5OWUwMDBkNDFmNzM2IiwiZXhwIjoxNzE3MTUyNjU5LCJpYXQiOjE3MTcxNTA4NTksImlzcyI6IjY2NTlhNDhiZTY5OTllMDAwZDMzZGFiYyIsImp0aSI6IjY2NTlhNDhiZTY5OTllMDAwZDMzZGFkOCIsInN0aXRjaF9kZXZJZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsInN0aXRjaF9kb21haW5JZCI6IjY2NThlZWFkZTY5OTllMDAwZDQxZjczNiIsInN1YiI6IjY2NTlhNDNlYjcyNjA2YjA1YjY3ZGM4YyIsInR5cCI6ImFjY2VzcyJ9.X5EADaxSFTCFW2pGwcaaFEpBj4lc52eEa5yiUiwJwlw"
+    );
+
+    const raw = JSON.stringify({
+      collection: "articles",
+      database: "eh9",
+      dataSource: "crlarrea",
+      projection: {
+        _id: 1,
+        title: 1,
+        shortDescription: 1,
+        authors: 1,
+        createdAt: 1,
+      },
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://eu-central-1.aws.data.mongodb-api.com/app/data-eoqcbhs/endpoint/data/v1/action/find",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setArticles(result.documents))
+      .catch((error) => console.error(error));
   };
+
   useEffect(() => {
     getArticles();
   }, []);
@@ -17,25 +50,25 @@ export const Blog = () => {
     <section className="blog">
       <h2>Latest</h2>
       <div>
-      {articles.map((entry) => {
-        return (
-          <article
-            key={entry._id}
-            onClick={() => {
-              navigate(`article/${entry._id}`);
-            }}
-          >
-            <h3>
-              {/* <Link to={`article/${entry._id}`}> */}
-              {entry.title}
-              {/* </Link> */}
-            </h3>
-            <p>{entry.shortDescription}</p>
-            <p>{entry.authors.join(/\s/)}</p>
-            <span>{entry.createdAt.replaceAll("-", ".")}</span>
-          </article>
-        );
-      })}
+        {articles.map((entry) => {
+          return (
+            <article
+              key={entry._id}
+              onClick={() => {
+                navigate(`article/${entry._id}`);
+              }}
+            >
+              <h3>
+                {/* <Link to={`article/${entry._id}`}> */}
+                {entry.title}
+                {/* </Link> */}
+              </h3>
+              <p>{entry.shortDescription}</p>
+              <p>{entry.authors.join(/\s/)}</p>
+              <span>{entry.createdAt.replaceAll("-", ".")}</span>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- src/utils/requests.jsx
+++ src/utils/requests.jsx
@@ -1,10 +1,38 @@
-const getData = async (url, method = "GET") => {
-  let data = await fetch(url, {
-    method: method,
-  })
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
-  return data;
+const getData = async () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append(
+    "Authorization",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiYWFzX2RldmljZV9pZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImJhYXNfZG9tYWluX2lkIjoiNjY1OGVlYWRlNjk5OWUwMDBkNDFmNzM2IiwiZXhwIjoxNzE3MTUyNjU5LCJpYXQiOjE3MTcxNTA4NTksImlzcyI6IjY2NTlhNDhiZTY5OTllMDAwZDMzZGFiYyIsImp0aSI6IjY2NTlhNDhiZTY5OTllMDAwZDMzZGFkOCIsInN0aXRjaF9kZXZJZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsInN0aXRjaF9kb21haW5JZCI6IjY2NThlZWFkZTY5OTllMDAwZDQxZjczNiIsInN1YiI6IjY2NTlhNDNlYjcyNjA2YjA1YjY3ZGM4YyIsInR5cCI6ImFjY2VzcyJ9.X5EADaxSFTCFW2pGwcaaFEpBj4lc52eEa5yiUiwJwlw"
+  );
+
+  const raw = JSON.stringify({
+    collection: "articles",
+    database: "eh9",
+    dataSource: "crlarrea",
+    projection: {
+      _id: 1,
+      title: 1,
+      shortDescription: 1,
+      authors: 1,
+      createdAt: 1,
+    },
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://eu-central-1.aws.data.mongodb-api.com/app/data-eoqcbhs/endpoint/data/v1/action/find",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => result)
+    .catch((error) => console.error(error));
 };
 
 export { getData };
