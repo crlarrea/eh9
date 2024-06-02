364beb91f832e48f711dfc8d53b93d4bc01dd96b
Christian Larrea <crlarrea@protonmail.com>
2024-06-02 13:17:07 +0200

    add animation to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,6 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import menuAnimation from "../assets/img/menu_animation.json";
 import { GiCoffeePot } from "react-icons/gi";
 import { GiTeapotLeaves } from "react-icons/gi";
 import { GiSandwich } from "react-icons/gi";
@@ -22,7 +24,7 @@ export const Menu = () => {
   const updateMenuView = (condition) => {
     let filteredResults = menu.filter((el) => el.type === condition);
     setMenuView(filteredResults);
-    setActive(condition)
+    setActive(condition);
   };
 
   useEffect(() => {
@@ -38,8 +40,7 @@ export const Menu = () => {
           return (
             <button
               key={button}
-              className={(active === button) ? 'active' :'inactive'}
-          
+              className={active === button ? "active" : "inactive"}
               onClick={(ev) => {
                 updateMenuView(button);
               }}
@@ -55,6 +56,20 @@ export const Menu = () => {
             </button>
           );
         })}
+      </article>
+      <article>
+        <Player
+          autoplay
+          loop
+          src={menuAnimation}
+          style={{ height: "300px", width: "300px" }}
+          speed={0.2}
+        >
+          <Controls
+            visible={false}
+            buttons={["play", "repeat", "frame", "debug"]}
+          />
+        </Player>
       </article>
       <article>
         <h3>{menuView.length !== 0 && menuView[0].type}</h3>
