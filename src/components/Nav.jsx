3c9a34f0ccb4a41ada6c6ec3f86ffc97732c6e4e
Christian Larrea <crlarrea@protonmail.com>
2024-06-16 10:13:03 +0200

    update nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,13 +4,38 @@ import { FiShoppingBag } from "react-icons/fi";
 import { Link, NavLink, Navigate } from "react-router-dom";
 
 export const Nav = () => {
+  const scrollToElement = (id) => {
+    const el = document.getElementById(id);
+    el.scrollIntoView();
+  };
   return (
     <nav>
       <Logo />
 
-      <Link to="/#ooncy" onClick={()=>{scrollTo('#ooncy')}}>OONCY</Link>
-      <Link to="/#find-us" onClick={()=>{scrollTo('#find-us')}}> find us</Link>
-      <Link to="/#news" onClick={()=>{scrollTo('#news')}}> news</Link>
+      <Link
+        to="/#ooncy"
+        onClick={() => {
+          scrollToElement("ooncy");
+        }}
+      >
+        OONCY
+      </Link>
+      <Link
+        to="/#find-us"
+        onClick={() => {
+          scrollToElement("find-us");
+        }}
+      >
+        find us
+      </Link>
+      <Link
+        to="/#news"
+        onClick={() => {
+          scrollToElement("news");
+        }}
+      >
+        news
+      </Link>
       <Link to="/about">about</Link>
 
       <div className="shopping-trolley">
